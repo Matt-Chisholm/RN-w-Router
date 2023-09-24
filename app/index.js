a2ead01961284c660d3c1be6e8be2da3dee01e81
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-09-24 10:58:00 -0700

    headerbtn

--- app/index.js
+++ app/index.js
@@ -1,9 +1,41 @@
-import { View, Text } from "react-native";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
 
-const App = () => (
-  <View>
-    <Text>Hello World</Text>
-  </View>
-);
+import { COLORS, icons, images, SIZES } from "../constants/index";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenheaderBtn,
+  Welcome,
+} from "../components/index";
+
+const App = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenheaderBtn iconUrl={icons.menu} dimension='60%' />
+          ),
+          headerRight: () => (
+            <ScreenheaderBtn iconUrl={icons.profile} dimension='100%' />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.padding }}>
+          <Welcome />
+          <Popularjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default App;
